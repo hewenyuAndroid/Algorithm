430c612ddf80ced8590b5cdbbb0b0067b58b4a9b
hewenyu <he875027547@126.com>
2024-03-11 16:56:35 +0800

    反转字符串

---
+++ src/main/java/com/hwy/kotlin/CH344.java
@@ -0,0 +1,44 @@
+package com.hwy.kotlin;
+
+import java.util.Arrays;
+
+/**
+ * 反转字符串
+ * <p>
+ * https://leetcode.cn/problems/reverse-string/description/
+ * <p>
+ * 编写一个函数，其作用是将输入的字符串反转过来。输入字符串以字符数组 s 的形式给出。
+ * <p>
+ * 不要给另外的数组分配额外的空间，你必须原地修改输入数组、使用 O(1) 的额外空间解决这一问题。
+ * <p>
+ * 输入：s = ["h","e","l","l","o"]
+ * 输出：["o","l","l","e","h"]
+ * <p>
+ * 输入：s = ["H","a","n","n","a","h"]
+ * 输出：["h","a","n","n","a","H"]
+ */
+public class CH344 {
+
+    public static void main(String[] args) {
+        char[] arr = new char[]{'h', 'e', 'l', 'l', 'o'};
+        reverseString(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    public static void reverseString(char[] s) {
+        if (s == null || s.length <= 1) {
+            return;
+        }
+
+        int left = 0;
+        int right = s.length - 1;
+        while (left < right) {
+            char temp = s[right];
+            s[right] = s[left];
+            s[left] = temp;
+            left++;
+            right--;
+        }
+    }
+
+}
